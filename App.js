a71bd4e71f30f396a65c80f266b48eb537d3170f
Steicy De La Torre <shechytorres@gmail.com>
2022-05-11 06:24:15 -0500

    primer commit

---
+++ App.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react'
+import {
+  SafeAreaView,
+  View,
+  Text,
+  StyleSheet,
+  Pressable,
+} from 'react-native'
+import Formulario from './src/components/Formulario'
+
+const App = () => {
+
+  const [modalVisible, setModalVisible] = useState(false)
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.titulo}>Administrador de Citas {''}
+        <Text style={styles.tituloBold}>Veterinaria</Text>
+      </Text>
+
+      <Pressable
+        style={styles.btnNuevaCita}
+        onPress={() => setModalVisible(!modalVisible)}
+      >
+        <Text
+          style={styles.btnTextoNuevaCita}
+        >Nueva Cita</Text>
+      </Pressable>
+
+      <Formulario
+        modalVisible={modalVisible}
+        setModalVisible={setModalVisible}
+      />
+
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#F3F4F6',
+    flex: 1,
+  },
+  titulo: {
+    textAlign: 'center',
+    fontSize: 30,
+    color: '#374151',
+    fontWeight: '600'
+  },
+  tituloBold: {
+    fontWeight: '900',
+    color: '#6D28D9',
+  },
+  btnNuevaCita: {
+    backgroundColor: '#6D28D9',
+    padding: 15,
+    marginTop: 30,
+    marginHorizontal: 20,
+    borderRadius: 10
+  },
+  btnTextoNuevaCita: {
+    textAlign: 'center',
+    color: '#FFF',
+    fontSize: 18,
+    fontWeight: '900',
+    textTransform: 'uppercase'
+  }
+})
+
+export default App;
---
+++ src/components/Formulario.js
@@ -0,0 +1,186 @@
+import React, { useState } from 'react';
+import {
+  Modal,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  TextInput,
+  View,
+  ScrollView
+} from 'react-native';
+import DatePicker from 'react-native-date-picker';
+import Pressable from 'react-native/Libraries/Components/Pressable/Pressable';
+
+const Formulario = ({ modalVisible, setModalVisible }) => {
+  
+  const [paciente, setPaciente] = useState('')
+  const [propietario, setPropietario] = useState('')
+  const [email, setEmail] = useState('')
+  const [telefono, setTelefono] = useState('')
+  const [fecha, setFecha] = useState(new Date())
+  const [sintomas, setSintomas] = useState('')
+
+  return (
+    <Modal animationType="slide" visible={modalVisible}>
+
+      <SafeAreaView style={styles.contenido}>
+        <ScrollView>
+          <Text style={styles.titulo}>
+            Nueva {''}
+            <Text style={styles.tituloBold}>Cita</Text>
+          </Text>
+
+          <Pressable
+            style={styles.btnCancelar}
+            onLongPress={() => setModalVisible(!modalVisible)}
+          >
+            <Text style={styles.btnCancelarTexto}>Cancelar</Text>
+          </Pressable>
+
+          <View style={styles.campo}>
+            <Text style={styles.label}>Nombre Paciente</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Nombre Paciente"
+              placeholderTextColor={'#666'}
+              value={paciente}
+              onChangeText={setPaciente}
+            />
+          </View>
+          <View style={styles.campo}>
+            <Text style={styles.label}>Nombre Propietario</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Nombre Propietario"
+              placeholderTextColor={'#666'}
+              value={propietario}
+              onChangeText={setPropietario}
+            />
+          </View>
+          <View style={styles.campo}>
+            <Text style={styles.label}>Email Propietario</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Email Propietario"
+              placeholderTextColor={'#666'}
+              keyboardType="email-address"
+              value={email}
+              onChangeText={setEmail}
+            />
+          </View>
+          <View style={styles.campo}>
+            <Text style={styles.label}>Teléfono Propietario</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Teléfono Propietario"
+              placeholderTextColor={'#666'}
+              keyboardType="number-pad"
+              value={telefono}
+              onChangeText={setTelefono}
+              maxLength={10}
+            />
+          </View>
+          <View style={styles.campo}>
+            <Text style={styles.label}>Fecha Alta</Text>
+            <View style={styles.fechaContenedor}>
+              <DatePicker
+                date={fecha}
+                locale='es'
+                onDateChange={ (date) => setFecha(date)}
+              />
+            </View>
+          </View>
+          <View style={styles.campo}>
+            <Text style={styles.label}>Sintomas</Text>
+            <TextInput
+              style={[styles.input, styles.input]}
+              placeholder="Sintomas Paciente"
+              placeholderTextColor={'#666'}
+              value={sintomas}
+              onChangeText={setSintomas}
+              multiline={true}
+              numberOfLines={4}
+            />
+          </View>
+
+          <Pressable
+            style={styles.btnNuevaCita}
+          >
+            <Text style={styles.btnNuevaCitaTexto}>Agregar paciente</Text>
+          </Pressable>
+
+        </ScrollView>
+      </SafeAreaView>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  contenido: {
+    backgroundColor: '#6D28D9',
+    flex: 1,
+  },
+  titulo: {
+    fontSize: 30,
+    fontWeight: '600',
+    textAlign: 'center',
+    marginTop: 30,
+    color: '#FFF',
+  },
+  tituloBold: {
+    fontWeight: '900',
+  },
+  btnCancelar: {
+    marginTop: 30,
+    backgroundColor: '#5827A4',
+    marginHorizontal: 30,
+    padding: 20,
+    borderRadius: 10
+  },
+  btnCancelarTexto: {
+    color: '#FFF',
+    textAlign: 'center',
+    fontWeight: '900',
+    fontSize: 16,
+    textTransform: 'uppercase'
+  },
+  campo: {
+    marginTop: 48,
+    marginHorizontal: 30,
+  },
+  label: {
+    color: '#FFF',
+    marginBottom: 10,
+    marginTop: 15,
+    fontSize: 20,
+    fontWeight: '600',
+  },
+  input: {
+    backgroundColor: '#FFF',
+    padding: 15,
+    borderRadius: 10,
+  },
+  sintomasInput: {
+    height: 100,
+  },
+  fechaContenedor: {
+    backgroundColor: '#FFF',
+    borderRadius: 10
+  },
+  btnNuevaCita: {
+    marginVertical: 50,
+    backgroundColor: '#F59E08',
+    paddingVertical: 15,
+    marginHorizontal: 30,
+    borderRadius: 10
+  },
+  btnNuevaCitaTexto: {
+    color: '5827A4',
+    textAlign: 'center',
+    fontWeight: '900',
+    fontSize: 16,
+    textTransform: 'uppercase',
+  }
+});
+
+export default Formulario;
